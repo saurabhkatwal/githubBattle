c06eeabb42058002e085967a13245f91bd34cea7
saurabh <saurabhkatwal@gmail.com>
2022-10-07 00:49:33 +0530

    added night mode to nopage component

--- src/components/NoPage.js
+++ src/components/NoPage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class NoPage extends Component {
   render() {
     return (
-      <div>
+      <div className={this.props.night?("nopage night"):("nopage")}>
         <h1>No such route</h1>
       </div>
     )
